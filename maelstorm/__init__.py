4e5dc24abc63cfe917f1b140d314a5080af2e1c0
Alymantara <J.V.Hernandez@soton.ac.uk>
2013-12-09 15:37:03 +0000

    Inital commit

---
+++ maelstorm/__init__.py
@@ -0,0 +1,10 @@
+""":mod:`maelstorm` -- It does cool things for Cataclsymic Variables
+"""
+
+from maelstorm import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ maelstorm/metadata.py
@@ -0,0 +1,17 @@
+""":mod:`my_module.metadata` --- Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'maelstorm'
+project = "maelstorm"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'It does cool things for Cataclsymic Variables'
+authors = ['Juan V. Hernandez Santisteban']
+authors_string = ', '.join(authors)
+emails = ['J.V.Hernandez@soton.ac.uk']
+license = 'MIT'
+copyright = '2013 ' + authors_string
+url = 'http://www.astro.soton.ac.uk/~jvhs1g12'
